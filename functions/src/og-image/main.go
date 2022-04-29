807e5f3937c7657a21a17605ae50e512c27eea30
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-04-29 09:49:31 +0100

    Finish image template function

--- functions/src/og-image/main.go
+++ functions/src/og-image/main.go
@@ -6,32 +6,43 @@ import (
 	"net/http"
 	"image"
 	"image/png"
-	"os"
 	"bytes"
-  
+	"embed"
+	"text/template"
+
 	"github.com/srwiley/oksvg"
 	"github.com/srwiley/rasterx"
 )
 
+//go:embed og-template.svg
+var f embed.FS
+
+type requestData struct {
+	Params map[string]string
+}
+
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	w, h := 1200, 630
 
-	in, err := os.Open("../../static/og-template.svg")
-	if err != nil {
-	  panic(err)
-	}
-	defer in.Close()
-  
-	icon, _ := oksvg.ReadIconStream(in)
+	in, _ := f.ReadFile("og-template.svg")
+	tmpl, err := template.New("template").Parse(string(in))
+	if err != nil { return nil, err }
+
+	var out bytes.Buffer
+	err = tmpl.Execute(&out, requestData{request.QueryStringParameters})
+	if err != nil { return nil, err }
+
+	icon, err := oksvg.ReadIconStream(&out)
+	if err != nil { return nil, err }
 	icon.SetTarget(0, 0, float64(w), float64(h))
 	rgba := image.NewRGBA(image.Rect(0, 0, w, h))
 	icon.Draw(rasterx.NewDasher(w, h, rasterx.NewScannerGV(w, h, rgba, rgba.Bounds())), 1)
-  
+
 	var buf bytes.Buffer
-  
+
 	err = png.Encode(&buf, rgba)
 	if err != nil {
-	  panic(err)
+	  return nil, err
 	}
 	return &events.APIGatewayProxyResponse{
 		StatusCode:        200,
