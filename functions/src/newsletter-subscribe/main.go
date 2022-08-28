88c90c6d768a3653249df8a947e7a8dce87de2b3
Jacob Tomlinson <jacob@tomlinson.email>
2022-08-28 21:33:44 +0100

    Add newsletter subscribe function

---
+++ functions/src/newsletter-subscribe/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/google/uuid"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Status struct {
+	Status string `json:"status"`
+}
+
+type Member struct {
+	Address    string `json:"address"`
+	Vars       string `json:"vars"`
+	Subscribed bool   `json:"subscribed"`
+	Upsert     bool   `json:"upsert"`
+}
+
+type Vars struct {
+	Token string `json:"token"`
+}
+
+type Message struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	HTML    string `json:"html"`
+}
+
+func createUser(email string, mailgunBaseURL string, mailgunKey string, token string) error {
+
+	vars, _ := json.Marshal(Vars{token})
+	jsonValue, _ := json.Marshal(Member{
+		Address:    email,
+		Vars:       string(vars),
+		Subscribed: false,
+		Upsert:     true,
+	})
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s%s", mailgunBaseURL, "/lists/newsletter@tomlinson.email/members"), bytes.NewBuffer(jsonValue))
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth("api", mailgunKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	return err
+}
+
+func sendVerificationEmail(email string, mailgunBaseURL string, mailgunKey string, token string) error {
+
+	jsonValue, _ := json.Marshal(Message{
+		From:    "Jacob Tomlinson (Newsletter) <jacob+newsletter@tomlinson.email",
+		To:      email,
+		Subject: "Verify your email address",
+		HTML:    fmt.Sprintf("Thank you for subscribing to my newsletter. Before I can add you to the mailing list please click <a href=\"https://jacobtomlinson.dev/.netlify/functions/newsletter-verify?email=%s&token=%s\">here</a> to verify your email address.", email, token),
+	})
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s%s", mailgunBaseURL, "/tomlinson.email/messages"), bytes.NewBuffer(jsonValue))
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth("api", mailgunKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	return err
+}
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+	email := request.QueryStringParameters["email"]
+	mailgunKey := os.Getenv("MAILGUN_API_KEY")
+	mailgunBaseURL := os.Getenv("MAILGUN_BASE_URL")
+	token := uuid.New().string()
+
+	err := createUser(email, mailgunBaseURL, mailgunKey, token)
+	if err != nil {
+		status, _ := json.Marshal(Status{"Unable to create user"})
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			Body:       string(status),
+		}, nil
+	}
+
+	err = sendVerificationEmail(email, mailgunBaseURL, mailgunKey, token)
+	if err != nil {
+		status, _ := json.Marshal(Status{"Unable to send verification email"})
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			Body:       string(status),
+		}, nil
+	}
+
+	status, _ := json.Marshal(Status{"Subscribed"})
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(status),
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
